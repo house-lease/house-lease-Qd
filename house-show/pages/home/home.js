7769c8edc46b5176dd5796995cf78ac65329ae92
dkj <dkjxyy123@163.com>
2020-04-27 00:30:51 +0800

    新增主页面，以及搜索自定义组件

---
+++ house-show/pages/home/home.js
@@ -0,0 +1,68 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      images:['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586408829082&di=920ecb05f8383a12e79143a6452c2c10&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F3f26a959c3a3f5fbb55903a0e1551a0377e94c5013ab9-ipKmXi_fw658',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586408829082&di=920ecb05f8383a12e79143a6452c2c10&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F3f26a959c3a3f5fbb55903a0e1551a0377e94c5013ab9-ipKmXi_fw658'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
