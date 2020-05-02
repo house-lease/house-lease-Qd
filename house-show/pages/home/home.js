b06c400ecc6560cccc6929d502aeffd0b44e703b
dkj <dkjxyy123@163.com>
2020-05-02 19:13:38 +0800

    新增获取房屋列表接口

--- house-show/pages/home/home.js
+++ house-show/pages/home/home.js
@@ -5,44 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-      address:"郑州",
+      address_:"郑州",
+      houseName:"",
+      price:"",
+      startValue:"",
       images:['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586408829082&di=920ecb05f8383a12e79143a6452c2c10&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F3f26a959c3a3f5fbb55903a0e1551a0377e94c5013ab9-ipKmXi_fw658',
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586408829082&di=920ecb05f8383a12e79143a6452c2c10&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F3f26a959c3a3f5fbb55903a0e1551a0377e94c5013ab9-ipKmXi_fw658'
     ],
     houses:[
-      {
-      id:"1",  
-      houseName:"我的小家",
-      houseType:"整租",
-      price:"1000",
-      address:"商丘",
-      image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588043426099&di=4e34b9d49a73a122e052c5a09eb68ea6&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Fnuomi%2Fpic%2Fitem%2Fd31b0ef41bd5ad6ed985964c8acb39dbb6fd3c13.jpg"
-    },
-    {
-      id:"2",  
-      houseName:"我的小家",
-    houseType:"整租",
-    price:"1000",
-    address:"商丘",
-    image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588043426099&di=4e34b9d49a73a122e052c5a09eb68ea6&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Fnuomi%2Fpic%2Fitem%2Fd31b0ef41bd5ad6ed985964c8acb39dbb6fd3c13.jpg"
-    },
-    {
-    id:"3",  
-    houseName:"我的小家",
-    houseType:"整租",
-    price:"1000",
-    address:"商丘",
-    image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588043426099&di=4e34b9d49a73a122e052c5a09eb68ea6&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Fnuomi%2Fpic%2Fitem%2Fd31b0ef41bd5ad6ed985964c8acb39dbb6fd3c13.jpg"
-    },
-    {
-    id:"4",
-    houseName:"我的小家",
-    houseType:"整租",
-    price:"1000",
-    address:"商丘",
-    image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588043426099&di=4e34b9d49a73a122e052c5a09eb68ea6&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Fnuomi%2Fpic%2Fitem%2Fd31b0ef41bd5ad6ed985964c8acb39dbb6fd3c13.jpg"
-    }
+  
   ]
   },
   // 自动有方法
@@ -50,12 +21,32 @@ Page({
     console.info(e.currentTarget.dataset.id);
 
   },
+   // 定义一个函数：作用是查询所有的房屋信息，返回的结果是json格式
+   getHouseList: function(){
+     var this_ = this;
+    wx.request({
+      url: 'http://localhost:8080/house/house/queryHouse', 
+      data: {
+        address: this_.data.address_,
+        houseLeaseName:this_.data.houseName,
+        price:this_.data.price,
+        startValue:this_.data.startValue
+      },
+      success(res) {
+        console.info(res.data);
+        this_.setData({
+          "houses": res.data
+        });
+      }
+    })
+},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 调用查询房屋的信息
+    this.getHouseList();
   },
 
   /**
