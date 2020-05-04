2d87184891f75e11c49c0048cc26f66a9c3b8be0
Pengpeng <peng110928@163.com>
2020-05-04 17:22:34 +0800

    消息页面

---
+++ house-show/pages/message/message.js
@@ -0,0 +1,89 @@
+// pages/message/message.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    user:[
+        {
+          id:1,
+          name:"我是谁",
+          information:"hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+          image:"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1124881678,3417344117&fm=26&gp=0.jpg",
+          time:"22:36"
+        },
+        {
+          id:2,
+          name:"鸡腿子",
+          information:"pppppppppppp",
+          image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588525947851&di=b3466ea7bf6488ba1579fa29f046058a&imgtype=0&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D95693490%2C752518729%26fm%3D214%26gp%3D0.jpg",
+          time:"22:00"
+        },
+        {
+          id:3,
+          name:"菠萝菠萝蜜",
+          information:"vvvvvvvvvvv",
+          image:"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1874752187,1869611550&fm=26&gp=0.jpg",
+          time:"21:00"
+        }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
