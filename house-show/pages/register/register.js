98a7fe3c797c2292611366b75107684d2f8f48cd
Pengpeng <peng110928@163.com>
2020-06-02 20:53:31 +0800

    提交

--- house-show/pages/register/register.js
+++ house-show/pages/register/register.js
@@ -1,13 +1,79 @@
 // pages/register/register.js
+var app = getApp();
+var liunxUrl=app.globalData.liunxUrl
+var localUrl=app.globalData.localUrl
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     
+     phone:"",
+     verification:""
   },
+phone(e){
+  console.info(e)
+  this.data.phone=e.detail.value
+},
+note(){
+  wx.request({
+    url: liunxUrl+'/house/user/phoneVerification',
+    data:{
+      phone:this.data.phone
+    },
+    success:res=>{
+      console.info(res.data)
+      this.data.verification=res.data.data
+      this.setData({
+        verification:res.data.data
+      })
+    }
+  })
+  
+},
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // phone(e){
+  //   // console.info(e.detail.value)
+  //   this.data.phone = e.detail.value
+  //   console.info(this.data.phone)
+  // },
+  // note(){
+  //   console.info("点击")
+  //   console.info(this.data.phone)
+  //   wx.request({
+  //     url:liunxUrl+'/house/user/phoneVerification',
+  //     data:{
+  //       phone:this.data.phone
+  //     },
+  //     success:res =>{
+  //       console.info(res.data)
+  //       this.data.verification=res.data.data
+  //       this.setData({
+  //         verification:res.data.data
+  //       })
+  //     }
+  //   })
+  // }
+  // ,
   /**
    * 生命周期函数--监听页面加载
    */
